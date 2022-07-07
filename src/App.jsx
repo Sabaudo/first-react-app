8bc556aae023a203f207b143f032839cff1df6fd
Sabaudo <gabrielsabaudo@gmail.com>
2022-07-07 18:39:02 -0300

    Adição do projeto inicial de ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Tweet } from "./components/Tweet";
+function App() {
+  
+    return (
+      <div>  
+        <Tweet text="tweet 1"/>
+        <Tweet text="tweet 2"/>
+        <Tweet text="tweet 3"/>
+      </div>
+    );
+
+}
+
+export default App
---
+++ src/components/Tweet.tsx
@@ -0,0 +1,9 @@
+type TweetProps = {
+    text: string;
+}
+
+export function Tweet (props: TweetProps) {
+    return (
+        <h2>{props.text}</h2>
+    );
+}
